0981f2a46fb3553385a398925ce752f2e8696e9c
flyingpianoman <53154125+flyingpianoman@users.noreply.github.com>
2021-03-11 20:55:22 +0100

    Add project files.

---
+++ MamRenewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MamRenewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
