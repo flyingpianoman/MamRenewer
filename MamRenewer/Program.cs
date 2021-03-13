bc52f2d810911d3e68eaeb7ac5932149e23b5bfb
flyingpianoman <53154125+flyingpianoman@users.noreply.github.com>
2021-03-13 20:08:48 +0100

    .

--- MamRenewer/Program.cs
+++ MamRenewer/Program.cs
@@ -46,7 +46,7 @@ namespace MamRenewer
                 .ConfigureAppConfiguration(cb =>
                 {
                     cb.AddJsonFile("appsettings.json");
-                    cb.AddJsonFile("appsettings.localdev.json");
+                    cb.AddJsonFile("appsettings.localdev.json", optional: false);
                     cb.AddEnvironmentVariables();
                 })
                 .ConfigureServices((hostContext, services) =>
